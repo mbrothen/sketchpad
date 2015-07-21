e14da4f35b93c2b6ea940438cf831140643b8b89
Matthew <mbrothen@gmail.com>
2015-07-20 22:17:14 -0500

    v0.1  Colors change on mouse enter.  Static 16x16 grid.  No other options added.

---
+++ script.js
@@ -0,0 +1,28 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+var rows = 16;
+var columns = 16;
+var $row = $("<div />", {
+   class: 'row'
+});
+var $square = $("<div />", {
+    class: 'square'
+});
+
+$(document).ready(function () {
+    //add columns to the the temp row object
+    for (var i = 0; i < columns; i++) {
+        $row.append($square.clone());
+    }
+    //clone the temp row object with the columns to the wrapper
+    for (var i = 0; i < rows; i++) {
+        $("#container").append($row.clone());
+    }
+   change();
+    function change() {
+    $('.square').mouseenter(function () {
+        $(this).css('background', '#0000f0');
+    });
+    }
+
+});
