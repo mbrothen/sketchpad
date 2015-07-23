054f7790c87d0813cd6063cd57b72775b695a1ac
Matthew <mbrothen@gmail.com>
2015-07-22 21:41:17 -0500

    Enabled custom grid resizing

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
-var rows = 16;
-var columns = 16;
-var $row = $("<div />", {
+rows = 16;
+columns = 16;
+$row = $("<div />", {
     class: 'row'
 });
-var $square = $("<div />", {
+$square = $("<div />", {
     class: 'square'
 });
 
-$(document).ready(function() {
-    //add columns to the the temp row object
-    for (var i = 0; i < columns; i++) {
+function canvas(rows) {
+    //create square size based on input or prefilled var
+    $('#container').empty();
+    $('#container').html("");
+
+    //add columns to the the row object
+    for (var i = 0; i < rows; i++) {
         $row.append($square.clone());
     }
-    //clone the temp row object with the columns to the wrapper
+    //clone the row object with the columns to the container
     for (var i = 0; i < rows; i++) {
         $("#container").append($row.clone());
     }
+    $('.square').css('width', (960 / rows));
+    $('.square').css('height', (960 / rows));
+    $('.row').css('height', (960 / columns));
+    console.log('rows' + rows + 'i' + i);
     change();
-    clear();
+}
 
-    function change() {
-        $('.square').mouseenter(function() {
-            $(this).css('background', '#0000f0');
-        });
+function change() {
+    //color background of squares on mouse enter and exit
+    $('.square').mouseenter(function() {
+        $(this).css('background', '#0000f0');
+    });
 
-        $('.square').mouseleave(function() {
-            $(this).css('background', '#0000f0');
+    $('.square').mouseleave(function() {
+        $(this).css('background', '#0000f0');
+    });
+}
+
+function clear() {
+    $('button').click(function() {
+            /*$('.square').css('background', '#FFFFFF');*/
+            $('container').html("");
+            var grid = prompt("Enter new grid size (1-128):", "32");
+            rows = grid;
+            columns = grid;
+            canvas(rows, columns);
+            console.log(grid);
         });
     }
 
-    function clear() {
-        $('button').click(function() {
-            $('.square').css('background', '#FFFFFF');
-        });
-    }
+$(document).ready(function() {
+canvas(rows);
+change();
+clear();
+
+
+
 });
